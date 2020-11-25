5c2a97f6e5474ef2692a1ac10d686b17657bc211
Christopher Dean <christopher.dean11@gmail.com>
2020-11-25 17:39:28 -0500

    fixed for new website

--- oilprices.py
+++ oilprices.py
@@ -1,10 +1,13 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def curOilPrice():
-    response = requests.get('http://rockinghamoil.com/')
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    response = requests.get('https://rockinghamoil.com',headers=headers)
     bs = BeautifulSoup(response.text, features='html.parser')
-    tag = bs.find('span', class_='customdailyprice')
-    pricetxt = tag.text
-    price = float(pricetxt.split('$')[1])
+    tag = bs.find('div', class_='et_pb_text_inner')
+    pricetxt = tag.next_element.text
+    price = re.findall('(?!\$)[\d]*\.[\d]*', pricetxt)
+    price = float(price[0])
     return price
